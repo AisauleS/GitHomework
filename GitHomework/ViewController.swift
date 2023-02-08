8214a6614dee2163aa92a52ed428b3ec2d61b069
AisauleS <aisaulesibatova@gmail.com>
2023-02-08 14:15:35 +0500

    Initial Commit

---
+++ GitHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHomework
+//
+//  Created by Aisaule Sibatova on 08.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
